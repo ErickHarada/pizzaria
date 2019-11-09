de554ee1be58e4006d6b3ff89eeb894d3b2b78ce
Erick <eefeaga2000@gmail.com>
2019-11-09 17:19:34 -0300

    Tela de pedido

--- src/Pedido.java
+++ src/Pedido.java
@@ -11,9 +11,15 @@ import factory.ConnectionFactory;
 
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import javax.swing.JComboBox;
+import javax.swing.SwingConstants;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
 
 public class Pedido extends JFrame {
 
@@ -22,6 +28,10 @@ public class Pedido extends JFrame {
 	private JScrollPane scroll;
 	private JTextField textCliente;
 	private JTextField textTelefone;
+	private JComboBox<String> comboBox;
+	ArrayList<Produto> estoque;
+	private JTextField textQuantidade;
+	private JButton btnCancelar;
 
 	public Pedido(String nome, String tel) {
 		setTitle("Pedido");
@@ -45,26 +55,79 @@ public class Pedido extends JFrame {
 		contentPane.add(scroll);
 
 		JLabel lblCliente = new JLabel("Cliente");
+		lblCliente.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblCliente.setFont(new Font("Arial", Font.BOLD, 15));
-		lblCliente.setBounds(27, 32, 56, 16);
+		lblCliente.setBounds(38, 32, 56, 16);
 		contentPane.add(lblCliente);
 
 		textCliente = new JTextField(nome);
 		textCliente.setFont(new Font("Arial", Font.BOLD, 15));
-		textCliente.setBounds(95, 25, 173, 30);
+		textCliente.setBounds(106, 25, 173, 30);
 		textCliente.setEditable(false);
 		contentPane.add(textCliente);
 
 		JLabel lblTelefone = new JLabel("Telefone");
+		lblTelefone.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblTelefone.setFont(new Font("Arial", Font.BOLD, 15));
-		lblTelefone.setBounds(12, 74, 71, 16);
+		lblTelefone.setBounds(23, 74, 71, 16);
 		contentPane.add(lblTelefone);
 
 		textTelefone = new JTextField(tel);
 		textTelefone.setFont(new Font("Arial", Font.BOLD, 15));
-		textTelefone.setBounds(95, 67, 173, 30);
+		textTelefone.setBounds(106, 67, 173, 30);
 		textTelefone.setEditable(false);
 		contentPane.add(textTelefone);
+
+		comboBox = new JComboBox<String>();
+		comboBox.setFont(new Font("Arial", Font.PLAIN, 15));
+		comboBox.setBounds(91, 179, 160, 22);
+		contentPane.add(comboBox);
+		comboBox.insertItemAt("", 0);
+		carregaLista();
+		
+		JLabel lblSabor = new JLabel("Sabor");
+		lblSabor.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSabor.setFont(new Font("Arial", Font.BOLD, 15));
+		lblSabor.setBounds(23, 182, 56, 16);
+		contentPane.add(lblSabor);
+		
+		JLabel lblQuantidade = new JLabel("Quantidade");
+		lblQuantidade.setFont(new Font("Arial", Font.BOLD, 15));
+		lblQuantidade.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblQuantidade.setBounds(263, 182, 82, 16);
+		contentPane.add(lblQuantidade);
+		
+		textQuantidade = new JTextField();
+		textQuantidade.setFont(new Font("Arial", Font.PLAIN, 15));
+		textQuantidade.setBounds(354, 179, 61, 22);
+		contentPane.add(textQuantidade);
+		textQuantidade.setColumns(10);
+		
+		JLabel lblBorda = new JLabel("Borda");
+		lblBorda.setFont(new Font("Arial", Font.BOLD, 15));
+		lblBorda.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblBorda.setBounds(427, 182, 56, 16);
+		contentPane.add(lblBorda);
+		
+		JCheckBox chckbxBorda = new JCheckBox("");
+		chckbxBorda.setFont(new Font("Tahoma", Font.PLAIN, 35));
+		chckbxBorda.setBounds(491, 179, 25, 25);
+		contentPane.add(chckbxBorda);
+		
+		btnCancelar = new JButton("Cancelar");
+		btnCancelar.setFont(new Font("Arial", Font.PLAIN, 13));
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+				Inicial inicial = new Inicial();
+				inicial.setVisible(true);
+				inicial.setLocationRelativeTo(null); // centraliza a janela
+			}
+		});
+		btnCancelar.setBounds(154, 384, 97, 30);
+		contentPane.add(btnCancelar);
+		
+		
 	}
 
 	public void mostraProduto() {
@@ -72,13 +135,20 @@ public class Pedido extends JFrame {
 			Connection connection = ConnectionFactory.getConnection();
 			connection.setAutoCommit(false);
 			Produto produto = new Produto();
-			ArrayList<Produto> estoque = produto.carregarProduto(connection);
+			estoque = produto.carregarProduto(connection);
 			TabelaProdutos tabela = new TabelaProdutos(estoque);
 			table.setModel(tabela);
 			connection.commit();
 			connection.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}
+	}
+
+	public void carregaLista() {
+		for (Produto p : estoque) {
+			String nomeProduto = p.getNome();
+			comboBox.addItem(nomeProduto);
 		}
 	}
 }
